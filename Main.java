70912ab609559b4ff5a29d32ba3ff1ac03ef6aa7
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-20 20:38:18 -0300

    Variables declaradas

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        /*
+        * 1. Declarar dos variables enteras y cargar sus valores por teclado.
+        * Informar su suma, diferencia, producto y cociente.
+        */
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Ingrese por teclado dos números enteros");
+        Integer miVariable01 = teclado.nextInt();
+        Integer miVariable02 = teclado.nextInt();
+    }
+}
