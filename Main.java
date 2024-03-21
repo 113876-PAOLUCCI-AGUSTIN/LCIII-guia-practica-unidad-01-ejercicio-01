bc0289d4a6c2510aa77950f7fd97a76a2cf9cc63
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-20 21:06:03 -0300

    Metodos creados.

--- Main.java
+++ Main.java
@@ -1,14 +1,27 @@
+/*
+ * 1. Declarar dos variables enteras y cargar sus valores por teclado.
+ * Informar su suma, diferencia, producto y cociente.
+ */
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        /*
-        * 1. Declarar dos variables enteras y cargar sus valores por teclado.
-        * Informar su suma, diferencia, producto y cociente.
-        */
         Scanner teclado = new Scanner(System.in);
         System.out.println("Ingrese por teclado dos números enteros");
         Integer miVariable01 = teclado.nextInt();
         Integer miVariable02 = teclado.nextInt();
+    }
+    
+    public static Integer sumarVariables(Integer primerNumero, Integer segundoNumero){
+        return primerNumero + segundoNumero;
+    }
+    public static Integer restarVariables(Integer primerNumero, Integer segundoNumero){
+        return primerNumero - segundoNumero;
+    }
+    public static Integer multiplicarVariables(Integer primerNumero, Integer segundoNumero){
+        return primerNumero * segundoNumero;
+    }
+    public static Integer dividirVariables(Integer primerNumero, Integer segundoNumero){
+        return primerNumero / segundoNumero;
     }
 }
