58bd7ede971687cfa8d71c1f9a0255cf291985ee
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-20 21:09:33 -0300

    Informe por pantalla creado.

--- Main.java
+++ Main.java
@@ -10,8 +10,14 @@ public class Main {
         System.out.println("Ingrese por teclado dos números enteros");
         Integer miVariable01 = teclado.nextInt();
         Integer miVariable02 = teclado.nextInt();
+        System.out.println("De acuerdo a los valores ingresados:");
+        System.out.println("La suma es: "+ sumarVariables(miVariable01,miVariable02));
+        System.out.println("La resta es: "+ restarVariables(miVariable01,miVariable02));
+        System.out.println("El producto es: "+ multiplicarVariables(miVariable01,miVariable02));
+        System.out.println("El cociente es: "+ dividirVariables(miVariable01,miVariable02));
     }
-    
+
+    // Métodos estáticos:
     public static Integer sumarVariables(Integer primerNumero, Integer segundoNumero){
         return primerNumero + segundoNumero;
     }
